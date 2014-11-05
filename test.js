28ae73ff64100d80a844a65d14f8c398831f47bf
matt <matt@clover.com>
2014-11-05 13:18:00 -0800

    publish() and subscribe() working without passing any arguments. should get unsubscribe() working next

--- test.js
+++ test.js
@@ -4,8 +4,107 @@ if (typeof require !== 'undefined') {
     global.tj = require('./tj');
 }
 
+beforeEach(function () {
+    tj.clearAllSubscriptions();
+});
+
 describe('sanity check', function () {
     it('tj should be defined', function () {
         expect(tj).to.be.ok();
     });
 });
+
+// basic publish / subscribe / unsubscribe mechanism
+
+describe('clearAllSubscriptions() tests', function () {
+    it('clearing subscriptions should prevent publishSync() from firing events', function () {
+        var ok = true;
+        
+        tj.subscribe('foo', function () {
+            ok = false;
+        });
+        
+        tj.clearAllSubscriptions();
+        tj.publishSync('foo');
+        
+        expect(ok).to.be(true);
+    });
+    
+    it('clearing subscriptions should cause all unsubscribe calls to return false', function () {
+        var fn = function () {};
+        
+        tj.subscribe('foo', fn);
+        tj.clearAllSubscriptions();
+        
+        expect(tj.unsubscribe(fn)).to.be(false);
+    });
+});
+
+describe('subscribe() and publishSync() tests', function () {
+    it('publishSync() with one event', function () {
+        var ok = false;
+        
+        tj.subscribe('foo', function () {
+            ok = true;
+        });
+        
+        tj.publishSync('foo');
+        
+        expect(ok).to.be(true);
+    });
+});
+
+describe('subscribe() and publish() tests', function () {
+    it('calling publish() before subscribe() on the same tick should register', function (done) {
+        var ok = false;
+        
+        tj.subscribe('foo', function () {
+            ok = true;
+        });
+        
+        tj.publish('foo');
+        
+        setTimeout(function () {
+            expect(ok).to.be(true);
+            
+            done();
+        }, 0);
+    });
+    
+    it('calling publish() after subscribe() on the same tick should register', function (done) {
+        var ok = false;
+        
+        tj.publish('foo');
+        
+        tj.subscribe('foo', function () {
+            ok = true;
+        });
+        
+        setTimeout(function () {
+            expect(ok).to.be(true);
+            
+            done();
+        }, 0);
+    });
+});
+
+describe('subscribe(), unsubscribe(), and publishSync() tests', function () {
+    it('subscribe -> unsubscribe -> publish should not register', function (done) {
+        var ok = true;
+        var token = tj.subscribe('foo', function () {
+            ok = false;
+        });
+        
+        tj.unsubscribe(token);
+        
+        tj.publish('foo');
+        
+        setTimeout(function () {
+            expect(ok).to.be(true);
+            
+            done();
+        }, 0);
+    });
+});
+
+// type checking
--- tj.js
+++ tj.js
@@ -1,25 +1,65 @@
 (function (tj, global) {
     'use strict';
 
+    var numTokensRequested = 0;
     var is = require('is-simple');
+    var allSubscribers = {}; // event -> {token: <string>, fn: <Function>}
+    
+    var getUniqueToken = (function () {
+        numTokensRequested += 1;
+        
+        return String(numTokensRequested);
+    });
+    
+    var emit = (function (event) {
+        if (allSubscribers.hasOwnProperty(event)) {
+            var eventSubscribers = allSubscribers[event];
+            
+            for (var i = 0; i < eventSubscribers.length; i++) {
+                var fn = eventSubscribers[i].fn;
+
+                try {
+                    fn();
+                } catch (e) {
+                    setTimeout(function () {
+                        throw e;
+                    }, 0);
+                }
+            }
+        }
+    });
     
     tj.clearAllSubscriptions = (function () {
-        
+        allSubscribers = {};
     });
 
     tj.publish = (function (event) {
-
+        setTimeout(function () {
+            emit(event);
+        }, 0);
     });
     
     tj.publishSync = (function (event) {
-        
+        emit(event);
     });
 
     tj.subscribe = (function (event, fn) {
-
+        var token = getUniqueToken();
+        
+        if (!allSubscribers.hasOwnProperty(event)) {
+            // this is the first subscriber to this event
+            allSubscribers[event] = [];
+        }
+        
+        allSubscribers[event].push({
+            token: token,
+            fn: fn
+        });
+        
+        return token;
     });
 
     tj.unsubscribe = (function (token) {
-
+        return false;
     });
 }(typeof exports === 'undefined' ? this.tj = {} : exports, this));
