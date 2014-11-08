3f239740fcd7e3d2908b17572b21e9ed31af4ece
Matt Oates <matthew.oates08@gmail.com>
2014-11-08 15:47:30 -0800

    fucking brackets

--- tj.js
+++ tj.js
@@ -4,17 +4,17 @@
     var numTokensRequested = 0;
     var is = require('is-simple');
     var allSubscribers = {}; // event -> {token: <string>, fn: <Function>}
-    
+
     var getUniqueToken = (function () {
         numTokensRequested += 1;
-        
+
         return String(numTokensRequested);
     });
-    
+
     var emit = (function (event, args) {
         if (allSubscribers.hasOwnProperty(event)) {
             var eventSubscribers = allSubscribers[event];
-            
+
             for (var i = 0; i < eventSubscribers.length; i++) {
                 var fn = eventSubscribers[i].fn;
 
@@ -28,7 +28,7 @@
             }
         }
     });
-    
+
 
     tj.clearAllSubscriptions = (function () {
         allSubscribers = {};
@@ -36,12 +36,12 @@
 
     tj.publish = (function (event) {
         var args = [].slice.call(arguments, 1, arguments.length);
-        
+
         setTimeout(function () {
             emit(event, args);
         }, 0);
     });
-    
+
     tj.publishSync = (function (event) {
         var args = [].slice.call(arguments, 1, arguments.length);
 
@@ -50,30 +50,30 @@
 
     tj.subscribe = (function (event, fn) {
         var token = getUniqueToken();
-        
+
         if (!allSubscribers.hasOwnProperty(event)) {
             // this is the first subscriber to this event
             allSubscribers[event] = [];
         }
-        
+
         allSubscribers[event].push({
             token: token,
             fn: fn
         });
-        
+
         return token;
     });
 
     tj.unsubscribe = (function (token) {
         var event;
-      
+
         for (event in allSubscribers) {
             if (allSubscribers.hasOwnProperty(event)) {
                 var eventSubscribers = allSubscribers[event];
-                
+
                 for (var i = 0; i < eventSubscribers.length; i++) {
                     var eventSubscriber = eventSubscribers[i];
-                    
+
                     if (eventSubscriber.token === token) {
                         eventSubscribers.splice(i, 1); // remove the subscriber
                         return true; // nothing else to do
@@ -81,7 +81,7 @@
                 }
             }
         }
-        
+
         return false;
     });
 }(typeof exports === 'undefined' ? this.tj = {} : exports, this));
